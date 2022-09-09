d2f59864005096312865b78a03c622d483b5b131
Amit <tiwamit44@gmail.com>
2022-09-09 13:37:37 +0530

    adding basic inMemory authentication to project

---
+++ src/main/java/com/amit/Practice/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.amit.Practice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+	public void configure( AuthenticationManagerBuilder auth  ) throws Exception
+	{
+		  auth.inMemoryAuthentication().withUser("amit").password("amit").roles("USER")
+		  .and().withUser("ajeesh").password("ajeesh").roles("ADMIN");
+	}
+	
+	
+	
+	
+	public void configure(HttpSecurity http) throws Exception
+	{
+		   http.httpBasic().and()
+		   .authorizeRequests().antMatchers(HttpMethod.GET,"/employees").hasAnyRole("ADMIN")
+		  .antMatchers(HttpMethod.DELETE,"/employee/delete/{id}").hasAnyRole("USER")
+		   .and().formLogin().and().csrf().disable();
+		   
+		   // httpbasic() is used if you are using postman vis basic auth
+		   
+		   //.csrf().disable() is used for DELETE,POST,PUT operation
+		   //we need to disable csrf protection as bydefault it is enable
+		   //in spring security for DELETE,POST,PUT operation.
+		   
+	}
+	
+	@Bean
+	public PasswordEncoder passwordIncoderInstance()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+}
